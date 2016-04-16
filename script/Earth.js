de77d544dcb8b3705a01d159ddfab4d1cecff8d5
Adeniyi Bello <niyipopp@yahoo.co.uk>
2016-04-16 14:55:09 +0100

    Earth material added
    The material orientation was due to a corrupt babylon.js file.

--- script/Earth.js
+++ script/Earth.js
@@ -1,87 +1,86 @@
-"use strict";
-
-var canvas;
-var engine;
-var scene;
 
 document.addEventListener("DOMContentLoaded", startBabylonJS, false);
 
 function startBabylonJS() {
   if (BABYLON.Engine.isSupported()) {
-    canvas = document.getElementById("renderCanvas");
-    engine = new BABYLON.Engine(canvas, true);
+    var canvas = document.getElementById("renderCanvas");
+    var engine = new BABYLON.Engine(canvas, true);
+
+    var scene = new BABYLON.Scene(engine);
+    scene.collisionsEnabled = true;
+
+    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, new BABYLON.Vector3(0, 0, 0), scene);
+    camera.setPosition(new BABYLON.Vector3(-15, 3, 0));
+    camera.attachControl(canvas);
+    camera.checkCollisions = true;
+
+    var light = new BABYLON.PointLight("Omni0", new BABYLON.Vector3(-17.6, 0, 0), scene);
+    var light2 = new BABYLON.PointLight("Omni1", new BABYLON.Vector3(-17.6, 10, 0), scene);
+    var light3 = new BABYLON.PointLight("Omni2", new BABYLON.Vector3(-17.6, -10, 0), scene);
+
+    light.diffuse = new BABYLON.Color3(1, 1, 1);
+    light.specular = new BABYLON.Color3(0, 0, 0);
+
+    light2.diffuse = new BABYLON.Color3(1, 1, 1);
+    light2.specular = new BABYLON.Color3(0, 0, 0);
+
+    light3.diffuse = new BABYLON.Color3(1, 1, 1);
+    light3.specular = new BABYLON.Color3(0, 0, 0);
+
+    var sun1 = BABYLON.Mesh.CreateSphere("sun", 10, 4, scene);
+    sun1.material = new BABYLON.StandardMaterial("sun", scene);
+    sun1.material.emissiveColor = new BABYLON.Color3(1, 1, 0);
+    sun1.position = light.position;
+
+    var sun2 = BABYLON.Mesh.CreateSphere("sun", 10, 4, scene);
+    sun2.material = new BABYLON.StandardMaterial("sun", scene);
+    sun2.material.emissiveColor = new BABYLON.Color3(1, 1, 0);
+    sun2.position = light2.position;
+
+    var sun3 = BABYLON.Mesh.CreateSphere("sun", 10, 4, scene);
+    sun3.material = new BABYLON.StandardMaterial("sun", scene);
+    sun3.material.emissiveColor = new BABYLON.Color3(1, 1, 0);
+    sun3.position = light3.position;
+    //var light = new BABYLON.HemisphericLight("light0", new BABYLON.Vector3(0, 1, 0), scene);
+    //var light2 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, -1, 0), scene);
+
+    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 20, 3, scene);
+    var sphere2 = BABYLON.Mesh.CreateSphere("sphere2", 20, 3, scene);
+    var sphere3 = BABYLON.Mesh.CreateSphere("sphere3", 20, 3, scene);
+
+    sphere.position.z = 0;
+    sphere2.position.z = 4;
+    sphere3.position.z = -4;
+
+    sphere.checkCollisions = true;
+    sphere2.checkCollisions = true;
+    sphere3.checkCollisions = true;
+
+    var mat = new BABYLON.StandardMaterial("default", scene);
+
+    mat.diffuseTexture = new BABYLON.Texture("material/earth.jpg", scene);
+    //mat.diffuseTexture.uScale = 2.0;
+    //mat.diffuseTexture.vScale = 2.0;
+
+        sphere.material = mat;
+        sphere2.material = mat;
+        sphere3.material = mat;
+
+    //var ground = BABYLON.Mesh.CreateGround("ground1", 100, 100, 2, scene);
+
+    engine.runRenderLoop(function () {
+        //scene.activeCamera.alpha += .01;
+        sphere.rotation.y += 0.03;
+        sphere2.rotation.y += 0.03;
+        sphere3.rotation.y += 0.03;
+        scene.render();
+    });
 
     // Resize
     window.addEventListener("resize", function () {
         engine.resize();
     });
 
-    // Scene, light and camera
-    var scene = new BABYLON.Scene(engine);
-    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
-    var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 2, 300, new BABYLON.Vector3(0, 0, 0), scene);
-    camera.attachControl(canvas);
-
-    // Assets manager
-    var assetsManager = new BABYLON.AssetsManager(scene);
-
-    //var meshTask = assetsManager.addMeshTask("skull task", "", "skull/", "skull.babylon");
-    //var meshTask2 = assetsManager.addMeshTask("skull task", "", "skull/", "skull.babylon");
-    //var meshTask3 = assetsManager.addMeshTask("skull task", "", "skull/", "skull.babylon");
-
-    // You can handle success and error on a per-task basis (onSuccess, onError)
-    //meshTask.onSuccess = function (task) {
-        //task.loadedMeshes[0].position = new BABYLON.Vector3(0, 0, 0);
-    //}
-    //meshTask2.onSuccess = function (task) {
-    //    task.loadedMeshes[0].position = new BABYLON.Vector3(70, 0, 0);
-    //}
-    //meshTask3.onSuccess = function (task) {
-    //    task.loadedMeshes[0].position = new BABYLON.Vector3(-70, 0, 0);
-    //}
-
-    //Earth
-    var sphere = BABYLON.Mesh.CreateSphere("Sphere", 20, 70, scene);
-
-    var sphere2 = BABYLON.Mesh.CreateSphere("Sphere", 20, 70, scene);
-    sphere2.position = new BABYLON.Vector3(-100, 0, 0);
-
-    var sphere3 = BABYLON.Mesh.CreateSphere("Sphere", 20, 70, scene);
-    sphere3.position = new BABYLON.Vector3(100, 0, 0);
-
-    var material = new BABYLON.StandardMaterial("kosh", scene);
-    material.bumpTexture = new BABYLON.Texture("material/normalMap.png", scene);
-
-    var mat = new BABYLON.StandardMaterial("earth_no_clouds", scene);
-    mat.diffuseTexture = new BABYLON.Texture("material/earth_no_clouds_2.jpg", scene);
-    //mat.diffuseTexture.uScale = 1.0;
-    //mat.diffuseTexture.vScale = 1.0;
-
-    sphere.material = material;
-    sphere2.material = mat;
-    sphere3.material = mat;
-
-
-    // But you can also do it on the assets manager itself (onTaskSuccess, onTaskError)
-    assetsManager.onTaskError = function (task) {
-        console.log("error while loading " + task.name);
-    }
-
-    assetsManager.onFinish = function (tasks) {
-        engine.runRenderLoop(function () {
-          //Camera to revolve
-          scene.activeCamera.alpha += .01
-
-          scene.render();
-        });
-    };
-
-
-
-    // Can now change loading background color:
     engine.loadingUIBackgroundColor = "Purple";
-
-    // Just call load to initiate the loading sequence
-    assetsManager.load();
   }
 }
